404310a9f60ac21e47827cc7bccc6589f9282676
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-10-16 10:03:06 -0600

    added a basic api fetch example

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 async function getCourses() {
-    return fetch(
-        "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json",
-        {mode: "no-cors"}
-    ) .then(function (response) {
-        return response.json();
-    });
+    try {
+        const response = await fetch(
+            "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json",
+            { mode: "cors" }
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching courses:", error);
+        throw error;
+    }
 }
 
 async function getCoursesDetails(coursesId) {
-    return fetch(
-        `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
-        {mode: "no-cors"}
-    ) .then(function (response) {
-        return response.json();
-    });
+    try {
+        const response = await fetch(
+            `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
+            { mode: "cors" }
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching course details:", error);
+        throw error;
+    }
 }
 
+// Example usage
+async function fetchData() {
+    try {
+        const courses = await getCourses();
+        console.log(courses);
+        
+        const courseDetails = await getCoursesDetails(11819);
+        console.log(courseDetails);
+    } catch (error) {
+        // Handle any errors here
+    }
+}
 
+fetchData();
 
 // other way;
-async function getAvailableCourses() {
-    const url = 'https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json';
-         const response = await fetch(url);
-        const data = await response.json();
+// async function getAvailableCourses() {
+//     const url = 'https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json';
+//          const response = await fetch(url);
+//         const data = await response.json();
         
-        return data;
-}    
-console.log(getCoursesDetails(11819))
+//         return data;
+// }    
+// console.log(getCoursesDetails(11819))
 // let courseOptions = '';
 // courses.forEach((course) => {
 //     courseOptions += `option value${}`
