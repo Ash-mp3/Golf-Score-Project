779859031542f831d35a245599ab8859abceadf6
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-10-16 09:25:47 -0600

    added a Golf Score header

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+async function getAvaliableCourses() {
+    const url= 'https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/';
+    const response = await fetch(url);
+const data = await response.json();
+return data;
+}
