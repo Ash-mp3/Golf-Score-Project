3e0cf4db5a369400ebd4f3f15496601ca34f9942
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-10-18 09:02:01 -0600

    fixed tee box selector

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 async function getCourses() {
-    try {
-        const response = await fetch(
-            "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json",
-            { mode: "cors" }
-        );
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching courses:", error);
-        throw error;
-    }
+  try {
+    const response = await fetch(
+      "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json",
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    throw error;
+  }
 }
 
 async function getCoursesDetails(coursesId) {
-    try {
-        const response = await fetch(
-            `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
-            { mode: "cors" }
-        );
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching course details:", error);
-        throw error;
-    }
+  try {
+    const response = await fetch(
+      `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching course details:", error);
+    throw error;
+  }
 }
-
 
 // Example usage
 async function fetchData() {
-    try {
-        // getting all courses with the api
-        const courses = await getCourses();
-        console.log(courses);
-        // getting thanksgiving points course data running the get courses details funciton.
-        
-        let courseOptionHTML = '';
-courses.forEach((course) => {
-courseOptionHTML += `<option value="${course.id}">${course.name}</option>`;
-});
-document.getElementById('selectedCourse').innerHTML = courseOptionHTML;
-    
-    } catch (error) {
-        // Handle any errors here 
-        console.error('fetchingCourses data did not happenin resulting in error:', error)
-    }
-    fetchCurrentGolfCourseURL();
+  try {
+    // getting all courses with the api
+    const courses = await getCourses();
+    console.log(courses);
+    // getting thanksgiving points course data running the get courses details funciton.
+
+    let courseOptionHTML = "";
+    courses.forEach((course) => {
+      courseOptionHTML += `<option value="${course.id}">${course.name}</option>`;
+    });
+    document.getElementById("selectedCourse").innerHTML = courseOptionHTML;
+  } catch (error) {
+    // Handle any errors here
+    console.error(
+      "fetchingCourses data did not happenin resulting in error:",
+      error
+    );
+  }
+  fetchCurrentGolfCourseURL();
 }
 
 fetchData();
@@ -53,45 +54,54 @@ fetchData();
 // steps needed for user options
 //make a golf corse select box. this will allow  you to select what course you want to use.
 // It will also go through the api, make a current list, and select that list and data.
-async function fetchCurrentGolfCourse (url) {
-try {
-   const response = await fetch(url);
-   if(!response) {
-       throw new Error('network response was not ok')
-   }
-   const currentGolfCourse = await response.json();
-   console.log(currentGolfCourse)
-   let teeBoxSelectHtml = '';
-   let totalYards = 0;
-         currentGolfCourse.holes.forEach((hole) => {hole.teeBoxes.forEach(function(teeBox) {
-            totalYards += teeBox.yards;
-            teeBoxSelectHtml += `<option value="${teeBox.teeType}">${teeBox.teeType.toUpperCase()}, ${teeBox.yards} yards</option>`;
-         });
-        });
-         document.getElementById('selectedTeeBox').innerHTML = teeBoxSelectHtml;
-   
-} catch(error) {
-console.error('Error:', error)
+async function fetchCurrentGolfCourse(url) {
+  try {
+    const response = await fetch(url);
+    if (!response) {
+      throw new Error("network response was not ok");
+    }
+    const currentGolfCourse = await response.json();
+    console.log(currentGolfCourse);
+    let teeBoxSelectHtml = "";
+    let totalYards = {};
+    currentGolfCourse.holes.forEach((hole) => {
+      hole.teeBoxes.forEach(function (teeBox) {
+        if (totalYards[teeBox.teeType] === undefined) {
+          totalYards[teeBox.teeType] = teeBox.yards;
+        } else {
+          totalYards[teeBox.teeType] += teeBox.yards;
+        }
+      });
+    });
+    for (let property in totalYards) {
+      if (totalYards.hasOwnProperty(property)) {
+        const yards = totalYards[property];
+        teeBoxSelectHtml += `<option value="${property}">${property.toUpperCase()}, ${yards} yards</option>`;
+      }
+    }
+    document.getElementById("selectedTeeBox").innerHTML = teeBoxSelectHtml;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
-}
-function fetchCurrentGolfCourseURL () {
-    //currentGolfCourseId is the slectedCourses id value wich is only 5 digits. input htis into the url to get complete access to the golf courses data.
-    let currentGolfCourseId = selectedCourse.value
-    let currentGolfCourseURL = `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${currentGolfCourseId}.json`
-    console.log(currentGolfCourseId)
-    console.log(currentGolfCourseURL)
-    
-    if(currentGolfCourseURL){
-    fetchCurrentGolfCourse(currentGolfCourseURL);
-    print();} 
-};
-function print () {
-   // print logic heref 
-}
+function fetchCurrentGolfCourseURL() {
+  //currentGolfCourseId is the slectedCourses id value wich is only 5 digits. input htis into the url to get complete access to the golf courses data.
+  let currentGolfCourseId = selectedCourse.value;
+  let currentGolfCourseURL = `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${currentGolfCourseId}.json`;
+  console.log(currentGolfCourseId);
+  console.log(currentGolfCourseURL);
 
+  if (currentGolfCourseURL) {
+    fetchCurrentGolfCourse(currentGolfCourseURL);
+    print();
+  }
+}
+function print() {
+  // print logic heref
+}
 
 //make a tee box select. after a user has selected a course make a display that allows the user to select what tee box they are playing on.
-    //currentGolfCourseURL is the current url selected.
+//currentGolfCourseURL is the current url selected.
 // this will send the id of the course sleected. then it will also send what tee box you want to access.
 //use the fetch word to do this.
 
