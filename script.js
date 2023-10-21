f00f732652096f14dc8ffbc8ffc8a2173e3670b8
Andrew Rasband <andrew.rasband4492@stu.mtec.edu>
2023-10-20 23:01:04 -0600

    trying to solve merge conflicts at midnight!

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ async function getCourses() {
   }
 }
 
-async function getCoursesDetails(coursesId) {
-  try {
-    const response = await fetch(
-      `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
-      { mode: "cors" }
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching course details:", error);
-    throw error;
-  }
-}
+// async function getCoursesDetails(coursesId) {
+//   try {
+//     const response = await fetch(
+//       `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${coursesId}.json`,
+//       { mode: "cors" }
+//     );
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching course details:", error);
+//     throw error;
+//   }
+// }
 
 // steps needed for user options
 //make a golf corse select box. this will allow  you to select what course you want to use.
@@ -103,9 +103,10 @@ function printTable() {}
 
 // run funciton on change or window load. put this into a window load function if we do local storage
 fetchData();
-document
-  .getElementById("selectedTeeBox")
-  .addEventListener("click", fetchCurrentGolfCourseURL());
+document.getElementById("selectedCourse").addEventListener('change', () => {
+    fetchCurrentGolfCourseURL();
+});
+
 // make a bootsraps responsive table with different labels. hole yardage par handicap. also have rows for players.
 // input current feild, with data sets in the print function
 
